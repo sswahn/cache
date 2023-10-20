558fa1f16852527b0776772363f669eba8f97b98
sswahn <36422765+sswahn@users.noreply.github.com>
2023-10-20 16:08:07 -0400

    Update cache.js

--- src/core/cache.js
+++ src/core/cache.js
@@ -5,36 +5,44 @@ const cacheUtility = {
       const response = await cache.match(request)
       return response.json()
     } catch (error) {
-      throw new Error(`Failed to parse response: ${name}`)
+      throw new Error(`Failed to retrieve from cache ${name}.`)
     }
   },
   async set(name, request, response) {
     try {
       const cache = await caches.open(name)
-      cache.put(request, response.clone())
+      await cache.put(request, response.clone())
     } catch (error) {
-      throw new Error(`Failed to set cache entry: ${name}`)
+      throw new Error(`Failed to set cache entry for ${name}.`)
     }
   },
   async delete(name, request) {
     try {
       const cache = await caches.open(name)
-      cache.delete(request)
+      await cache.delete(request)
     } catch (error) {
-      throw new Error(`Failed to delete cache: ${name}`)
+      throw new Error(`Failed to delete cache entry from ${name}.`)
     }
   },
   async clear(name) {
-    const cache = await caches.open(name)
-    const keys = await cache.keys()
-    for (let key of keys) {
-      await cache.delete(key)
+    try {
+      const cache = await caches.open(name)
+      const keys = await cache.keys()
+      for (let key of keys) {
+        await cache.delete(key)
+      }
+    } catch (error) {
+      throw new Error(`Failed to clear cache ${name}.`)
     }
   },
   async keys(name) {
-    const cache = await caches.open(name)
-    return cache.keys()
+    try {
+      const cache = await caches.open(name)
+      return cache.keys()
+    } catch (error) {
+      throw new Error(`Failed to retrieve keys from cache ${name}.`)
+    }
   }
 }
 
-export default cacheUtility
+export default cacheUtility;
