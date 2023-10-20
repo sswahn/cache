6c414c0892ad1bb67ddcbbadd1b4bc7c0c1051c8
sswahn <36422765+sswahn@users.noreply.github.com>
2023-10-20 16:03:04 -0400

    Update cache.js

--- src/core/cache.js
+++ src/core/cache.js
@@ -1,16 +1,39 @@
 const cacheUtility = {
   async get(name, request) {
-    const cache = await caches.open(name)
-    const response = await cache.match(request)
-    return response ? response.json() : null
+    try {
+      const cache = await caches.open(name)
+      const response = await cache.match(request)
+      return response.json()
+    } catch (error) {
+      throw new Error(`Failed to parse response: ${name}`)
+    }
   },
   async set(name, request, response) {
-    const cache = await caches.open(name)
-    cache.put(request, response.clone())
+    try {
+      const cache = await caches.open(name)
+      cache.put(request, response.clone())
+    } catch (error) {
+      throw new Error(`Failed to set cache entry: ${name}`)
+    }
   },
   async delete(name, request) {
+    try {
+      const cache = await caches.open(name)
+      cache.delete(request)
+    } catch (error) {
+      throw new Error(`Failed to delete cache: ${name}`)
+    }
+  },
+  async clear(name) {
     const cache = await caches.open(name)
-    cache.delete(request)
+    const keys = await cache.keys()
+    for (let key of keys) {
+      await cache.delete(key)
+    }
+  },
+  async keys(name) {
+    const cache = await caches.open(name)
+    return cache.keys()
   }
 }
 
